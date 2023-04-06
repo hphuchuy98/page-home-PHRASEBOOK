c8003b7e526c2abfe078418e87c512e0691c4919
hphuchuy98@gmail.com <hphuchuy98@gmail.com>
2023-04-06 09:24:57 +0700

    update hover

---
+++ js/show-menu.js
@@ -0,0 +1,33 @@
+const arrows = document.querySelectorAll(".arrow")
+const showSubMenu = document.querySelector('.sub-menu-phone')
+const addRotate = document.querySelector('.menu-phone-icon')
+const hideSubMenu = document.querySelector('.menu-text')
+
+
+function show_Submenu () {
+    showSubMenu.classList.add('show-menu');
+}
+
+function hide_Submenu () {
+    showSubMenu.classList.remove('show-menu')
+}
+
+
+function add_Rotate () {
+    addRotate.classList.add('rotate');
+}
+
+function remove_Rotate () {
+    addRotate.classList.remove('rotate');
+}
+
+
+for (const arrow of arrows ) {
+    arrow.addEventListener('click', show_Submenu);
+    arrow.addEventListener('click', add_Rotate);
+}
+
+hideSubMenu.addEventListener('click', hide_Submenu);
+hideSubMenu.addEventListener('click', remove_Rotate);
+
+
